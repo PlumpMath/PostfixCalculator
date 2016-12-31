27b12f14f460c75a6e9e5a57f848117e4df239a8
mindoo <leon2@sautour.net>
2016-12-31 17:11:04 +0100

    Finished coding popCalc, calculate() is almost done, TODO: print out the result

--- main.cpp
+++ main.cpp
@@ -3,7 +3,7 @@
 
 //Prototypes
 double calculate(std::stack postfixStack);
-bool isEmpty(std::string input);
+void popCalc(std::string str, std::stack<double>* postfixStack)
 
 int main() {
 
@@ -46,19 +46,67 @@ double calculate(std::stack postfixStack) {
     }
 
     //Check if the character is a space
-    if (var == " ") {
+    if (str == " ") {
       //Start back at the top of the loop
       return;
-    } else if (var == "+" || var == "-"|| var == "/"|| var == "*") { //Check if its an operator
-      //Call the calculating function
+    } else if (str == "+" || str == "-"|| str == "/"|| str == "*") { //Check if its an operator
+      //Call the calculating function with a pointer to
+      //the postfixStack
+      popCalc(str, &postfixStack);
+    } else {
+      //Convert the number to a double from a string
+      double converted;
+      converted = std::stod(str);
 
+      //Push the number into the stack
+      postfixStack.push(converted);
     }
+
+    //TODO: Return the result
 
   }
 
   return 0;
 }
 
-void popCalc(char var, std::stack<double>) {
+void popCalc(std::string str, std::stack<double>* postfixStack) {
+  //Operands
+  double operand1;
+  double operand2;
+
+  //NOTE: I'll have to test out this system while dividing as it might take the operands int the wrong order
+
+  //Pop out the Operands
+  operand1 = postfixStack.pop();
+  operand2 = postfixStack.pop();
+
+  //Store the result
+  double result;
+
+  //Determine what operation to do
+  switch (var) {
+    case "+": {
+      result = operand1 + operand2;
+      break;
+    }
+
+    case "-": {
+      result = operand1 - operand2;
+      break;
+    }
+
+    case "*": {
+      result = operand1 * operand2;
+      break;
+    }
+
+    case "/": {
+      result = operand1 / operand2;
+      break;
+    }
+  }
+
+  //Push the result in the postfixStack
+  postfixStack.push(result);
 
 }
